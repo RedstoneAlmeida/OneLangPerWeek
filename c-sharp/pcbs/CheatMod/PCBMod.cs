fcc5a4a5e26679520578c99b059e05f8427268ad
RedstoneAlmeida <mineps3marcos@gmail.com>
2020-01-24 01:09:24 -0300

    Add files via upload

--- c-sharp/pcbs/CheatMod/PCBMod.cs
+++ c-sharp/pcbs/CheatMod/PCBMod.cs
@@ -40,12 +40,16 @@ namespace PCBMod
 
         public override void Update()
         {
-
             CareerStatus status = CareerStatus.Get();
+            CareerStatus.State state = CareerStatus.GetState();
                            
             if (Input.GetKeyDown(KeyCode.F))
             {
-                CareerStatus.GetState().m_cash = 999999;
+                if(state.m_cash > 999999)
+                {
+                    return;
+                }
+                status.AddCash(999999);
             }
 
             if (Input.GetKeyDown(KeyCode.G))
@@ -55,11 +59,26 @@ namespace PCBMod
 
             if (Input.GetKeyDown(KeyCode.V))
             {
-                WorkshopController controller = WorkshopController.Get();
-                Transform f = controller.playerTransform.transform;
-                f.position = new Vector3(288.9f + NextFloat(new Random()), 12 + NextFloat(new Random()), 298.1f + NextFloat(new Random()));
-                controller.SetPlayerPos(f);
+                this.AddReview(status, state, new ReviewModded());
             }
+
+            if (Input.GetKeyDown(KeyCode.K))
+            {
+               
+            }
+        }
+
+        public override void OnGUI()
+        {
+            GUIStyle style = new GUIStyle();
+            style.normal.textColor = Color.cyan;
+            style.fontSize = 20;
+            style.fontStyle = FontStyle.Bold;
+            style.alignment = TextAnchor.UpperCenter;
+            GUI.Label(new Rect(Screen.width / 2 - 100, 10f, 200f, 30f),
+                "CheatMod Debug\n" +
+                "Cash = F | XP = G\n" +
+                "Review = V", style);
         }
 
         static float NextFloat(Random random)
@@ -68,6 +87,52 @@ namespace PCBMod
             // choose -149 instead of -126 to also generate subnormal floats (*)
             double exponent = Math.Pow(2.0, random.Next(-126, 128));
             return (float)(mantissa * exponent);
+        }
+
+        private void AddReview(CareerStatus status, CareerStatus.State state, IReview job)
+        {
+            if (!status.UnlockedReviews() && !(job is DummyReview))
+            {
+                return;
+            }
+            state.m_reviews.Insert(0, job);
+            float num = 0f;
+            int num2 = Mathf.Min(state.m_reviews.Count, CareerConstants.s_numberReviewsForStarRating);
+            for (int i = 0; i < num2; i++)
+            {
+                num += state.m_reviews[i].GetStars();
+            }
+            state.m_starRating = num / (float)num2;
+            Achievements.OnStarRating(state.m_starRating);
+
+            PlayerStatus playerStatus = UnityEngine.Object.FindObjectOfType<PlayerStatus>();
+            if(playerStatus && playerStatus.m_starRating)
+            {
+                playerStatus.m_starRating.SetRating(status.GetStarRating());
+            }
+        }
+
+        private void GenerateJob(JobDesc desc)
+        {
+            CareerStatus careerStatus = CareerStatus.Get();
+            int num = 0;
+            Job job;
+            for(; ; )
+            {
+                job = desc.Generate(careerStatus.GetNextJobId());
+                if (job.IsValid())
+                {
+                    break;
+                }
+                if(num++ == 10)
+                {
+                    goto Block_2;
+                }
+            }
+            careerStatus.AddJob(job);
+            return;
+            Block_2:
+                ModLogs.Log("Job don't created. 10 tries");
         }
     }
 }
---
+++ c-sharp/pcbs/CheatMod/ReviewModded.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using PCBSModloader;
+using PCBS;
+
+namespace PCBMod
+{
+    class ReviewModded : IReview
+    {
+
+        private Random random = new Random();
+        String[] reviews = {
+            "Best Cases! My Custom PC is VERY FAST!",
+            "No have best...",
+            "Best Assistence!",
+            "Cable Management is better!",
+            "The Best PC",
+            "I'm Happy using my new PC!",
+            "Modded PC, Very Good! Thanks! "
+        };
+
+        public int GetAvatarId()
+        {
+            return 1;
+        }
+
+        public int GetDay()
+        {
+            return random.Next(0, 20);
+        }
+
+        public string GetFrom()
+        {
+            return "cheat_modded@modded.com";
+        }
+
+        public string GetReview()
+        {
+            return reviews[random.Next(0, reviews.Length - 1)];
+        }
+
+        public float GetStars()
+        {
+            return 5;
+        }
+    }
+}
